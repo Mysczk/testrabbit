4214c2829a1eb3113f4718d7fde9744268192e8f
Mysczk <michal.soukup.jr@gmail.com>
2025-05-03 12:33:41 +0200

    Refactor of vect_calc.py

--- vect_calc.py
+++ vect_calc.py
@@ -3,15 +3,14 @@ import numpy as np
 class Calculator:
     def add(a, b):
         return np.add(a, b)
-    
-    def subtract( a, b):
+
+    def subtract(a, b):
         return np.subtract(a, b)
-    
-    def multiply( a, b):
+
+    def multiply(a, b):
         return np.multiply(a, b)
-    
-    def divide( a, b):
+
+    def divide(a, b):
         if np.any(b == 0):
             raise ValueError("Cannot divide by zero")
         return np.divide(a, b)
-    
